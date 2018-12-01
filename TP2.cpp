abf30c3a1e84947424a3460fed597e7a94501d4f
Alessandro5x <44681007+Alessandro5x@users.noreply.github.com>
2018-12-01 17:39:45 -0200

    Add files via upload

---
+++ TP2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "TP2.h"
+
+using namespace std;
+
+Usuario::Usuario(string n, string cp, string ende, string fon){
+    nome = n;
+    cpf = cp;
+    endereco = ende;
+    fone = fon;
+
+}
+
---
+++ TP2.h
@@ -0,0 +1,22 @@
+#ifndef TP2_H
+#define TP2_H
+
+class Usuario{
+
+    private:
+        string nome;
+        string cpf;
+        string endereco;
+        string fone;
+
+    public:
+        Usuario(string n, string cp, string ende, string fon);
+};
+class Date{
+private:
+    int dia;
+    int mes;
+    int ano;
+};
+
+#endif // TP2_H
